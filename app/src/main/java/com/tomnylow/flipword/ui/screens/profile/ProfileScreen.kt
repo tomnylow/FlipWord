0a0b9da208a63cdc34f55d2142b36fd079ded0f4
tpuser <tp@mail.ed>
2025-12-20 17:58:22 +0300

    add profile screen

---
+++ app/src/main/java/com/tomnylow/flipword/ui/screens/profile/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package com.tomnylow.flipword.ui.screens.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen() {
+    Text(text="Profile")
+}
