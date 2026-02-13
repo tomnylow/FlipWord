96718457347b48794d08b91aa230f51c76dcb2d3
vniuser <vni@user>
2026-02-13 13:50:03 +0300

    feat: offload db answer filtering to IO dispatcher

--- app/src/main/java/com/tomnylow/flipword/domain/usecase/card/GetDueCardsForDeckUseCase.kt
+++ app/src/main/java/com/tomnylow/flipword/domain/usecase/card/GetDueCardsForDeckUseCase.kt
@@ -3,7 +3,9 @@ package com.tomnylow.flipword.domain.usecase.card
 import com.tomnylow.flipword.domain.model.Card
 import com.tomnylow.flipword.domain.repository.CardRepository
 import com.tomnylow.flipword.domain.sm2.SM2Algorithm.isTimeForReview
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -14,6 +16,6 @@ class GetDueCardsForDeckUseCase @Inject constructor(
     operator fun invoke(deckId: Long): Flow<List<Card>> {
         return cardRepository.getCardsForDeck(deckId).map { cards ->
             cards.filter { it.isTimeForReview() }
-        }
+        }.flowOn(Dispatchers.IO)
     }
 }
